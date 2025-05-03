126561a5fe5a38c3be9a1d710b9ba64f290bf6a9
Ahmad Mugheera <tast.test.boy@gmail.com>
2025-05-03 12:56:48 +0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ GREEN='\033[0;32m'
 YELLOW='\033[0;33m'
 RESET='\033[0m'
 
-tools=("go" "httpx")
+tools=("go" "httpx" "uro")
 
 for tool in "${tools[@]}"; do
   if ! command -v "$tool" > /dev/null 2>&1; then
@@ -21,6 +21,10 @@ for tool in "${tools[@]}"; do
         echo -e "${YELLOW}[>] Installing httpx...⏳${RESET}"
         go install -v github.com/projectdiscovery/subfinder/v2/cmd/subfinder@latest
         cp ~/go/bin/subfinder /usr/bin
+        ;;
+      uro)
+        echo -e "${YELLOW}[>] Installing uro...⏳${RESET}"
+        pip3 install uro
         ;;
       *)
         echo -e "${YELLOW}[!] No install logic for $tool. Skipping...${RESET}"
